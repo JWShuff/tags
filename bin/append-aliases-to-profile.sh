ec082e8824e40558f44f07670abfbb263df145f9
Marc Stanley <marcson909@gmail.com>
2021-07-15 11:08:34 -0500

    added tags to django settings

--- bin/append-aliases-to-profile.sh
+++ bin/append-aliases-to-profile.sh
@@ -3,6 +3,6 @@ then
   sh=`basename $SHELL`
   profile=~/.${sh}_profile
   echo >> $profile  # In case the profile doesn't end with a newline
-  cat git-aliases.sh >> $profile
+  cat bin/git-aliases.sh >> $profile
   echo Appended git aliases to $profile
 fi
